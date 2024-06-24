111dc0692b8ef64c277578338dc52cdca0c32356
Poulo Malebo <poulo.malebo@standardbank.co.za>
2024-06-24 08:19:42 +0200

    Initial Commit

---
+++ FarmManagementApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FarmManagementApp
+//
+//  Created by Malebo, Poulo P on 2024/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FarmManagementApp/FarmManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmManagementAppApp.swift
+//  FarmManagementApp
+//
+//  Created by Malebo, Poulo P on 2024/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FarmManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
